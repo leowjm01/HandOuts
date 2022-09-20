473cfcf6d6e01110e35f5f149e710cd30ea53ff5
anreh <anurehka@gmail.com>
2022-09-20 10:53:13 +0800

    nav

---
+++ app/src/main/java/com/example/handouts/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.handouts.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.handouts.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        findNavController().navigate(R.id.action_mainFragment5_to_blankFragment2)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/handouts/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.handouts.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
